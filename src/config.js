95dbb6140f357355b215a94137b7339c7d82a34e
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-02-23 18:28:17 +0100

    send mail1

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-mahtha const config = {
+const config = {
     // basename: only at build time to set, and don't add '/' at end off BASENAME for breadcrumbs, also don't put only '/' use blank('') instead,
     // like '/berry-material-react/react/default'
     basename: '',
@@ -9,5 +9,3 @@ mahtha const config = {
 };
 
 export default config;
-
-lahtha
