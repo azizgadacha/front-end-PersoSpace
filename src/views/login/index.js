45beb432803287826e1a64fd5883de931fb02413
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-03-10 02:25:26 +0100

    bara

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -91,14 +91,7 @@ const Login = () => {
                                     </Grid>
                                     <Grid item xs={12}>
                                         <Grid item container direction="column" alignItems="center" xs={12}>
-                                            <Typography
-                                                component={RouterLink}
-                                                to="/register"
-                                                variant="subtitle1"
-                                                sx={{ textDecoration: 'none' }}
-                                            >
-                                       DON'T HAVE AN ACCOUNT ?  CLICK HERE
-                                            </Typography>
+                                            
                                         </Grid>
                                     </Grid>
                                 </Grid>
