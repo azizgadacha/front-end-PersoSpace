40fd0b203d0065caa2cf5d2083df22e42d83a430
Ouessim <aboucha.ouessim123@gmail.com>
2022-04-11 23:33:29 +0100

    modal share space

--- src/views/modal/ShareWorkspaceModal/index.js
+++ src/views/modal/ShareWorkspaceModal/index.js
@@ -56,6 +56,11 @@ import useScriptRef from "../../../hooks/useScriptRef";
 import {strengthColor, strengthIndicator} from "../../../verification_password/password-strength";
 import {makeStyles} from "@material-ui/styles";
 import SaveIcon from "@mui/icons-material/Save";
+import {UserListHead} from "../../ViewAll/import/customer/@dashboard/user";
+
+import SearchNotFound from "../../ViewAll/import/customer/SearchNotFound";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import Cells from "./Cells";
 
 // ----------------------------------------------------------------------
 
@@ -102,7 +107,7 @@ const useStyles = makeStyles((theme) => ({
         left: '50%',
 
         transform: 'translate(-50%, -50%)',
-        width: 600,
+        width: 1000,
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
@@ -235,6 +240,112 @@ const ShareWorkspaceModal=  (props) => {
     }, []);
 
 
+    const TABLE_HEAD = [
+        { id: 'username', label: 'User name', alignRight: false },
+        { id: 'email', label: 'Email', alignRight: false },
+        { id: 'phone', label: 'Phone', alignRight: false },
+        { id: 'role', label: 'Role', alignRight: false },
+
+        {  id: 'action', label: '           Activites', alignLeft: true }
+    ];
+    function descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+    function getComparator(order, orderBy) {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+    function applySortFilter(array, comparator, query) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        if (query) {
+            return filter(array, (_user) => (_user.phone).toString().indexOf(query.toLowerCase()) !== -1||_user.username.toLowerCase().indexOf(query.toLowerCase()) !== -1||_user.email.toLowerCase().indexOf(query.toLowerCase()) !== -1||_user.email.toLowerCase().indexOf(query.toLowerCase()) !== -1||_user.role.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+        }
+        return stabilizedThis.map((el) => el[0]);
+    }
+
+    const [page, setPage] = useState(0);
+    const [order, setOrder] = useState('asc');
+    const [selected, setSelected] = useState([]);
+    const [orderBy, setOrderBy] = useState('username');
+    const [filterName, setFilterName] = useState('');
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = userSt.users.map((n) => n.username);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1)
+            );
+        }
+        setSelected(newSelected);
+    };
+
+
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleFilterByName = (event) => {
+        setFilterName(event.target.value);
+    };
+    let userSt= useSelector((state) => state.user);
+
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userSt.users.length) : 0;
+
+    const filteredUsers = applySortFilter(userSt.users, getComparator(order, orderBy), filterName);
+
+    const isUserNotFound = filteredUsers.length === 0;
+
+    let open = useSelector((state) => state.modal);
+    function handleCloseModal  () {
+        dispatcher({
+            type:CLOSE_DELETE_MODAL,
+
+        });
+    };
+
+
 
 
 
@@ -297,7 +408,77 @@ const ShareWorkspaceModal=  (props) => {
 
                         <Box sx={{ ...style,  }} className={classes.modal}>
                             <ThemeConfig>
+                                <PerfectScrollbar>
+                                    <TableContainer sx={{minWidth: 800}}>
+                                        <Table>
+                                            <UserListHead
+                                                order={order}
+                                                orderBy={orderBy}
+                                                headLabel={TABLE_HEAD}
+                                                rowCount={userSt.users.length}
+                                                numSelected={selected.length}
+                                                onRequestSort={handleRequestSort}
+                                                onSelectAllClick={handleSelectAllClick}
+                                            />
+                                            <TableBody>
+                                                {filteredUsers
+                                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                                    .map((row) => {
 
+
+                                                        const {_id, username, email, phone,role,photo} = row;
+                                                        const isItemSelected = selected.indexOf(username) !== -1;
+
+                                                        return (
+                                                            <Fragment>
+                                                                <TableRow
+                                                                    hover
+                                                                    key={_id}
+                                                                    tabIndex={-1}
+                                                                    role="checkbox"
+                                                                    selected={isItemSelected}
+                                                                    aria-checked={isItemSelected}
+                                                                >
+                                                                    <TableCell padding="checkbox">
+                                                                        <Checkbox
+                                                                            checked={isItemSelected}
+                                                                            onChange={(event) => handleClick(event,username)}
+                                                                        />
+                                                                    </TableCell>
+                                                                    <Cells    userPar={{_id,username,phone,role,photo,email}}/>
+                                                                </TableRow>
+
+                                                            </Fragment>
+                                                        );
+                                                    } )}
+                                                {emptyRows > 0 && (
+                                                    <TableRow style={{height: 53 * emptyRows}}>
+                                                        <TableCell colSpan={6}/>
+                                                    </TableRow>
+                                                )}
+
+                                            </TableBody>
+                                            {isUserNotFound && (
+                                                <TableBody>
+                                                    <TableRow>
+                                                        <TableCell align="center" colSpan={6} sx={{py: 3}}>
+                                                            <SearchNotFound searchQuery={filterName}/>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                </TableBody>
+                                            )}
+                                        </Table>
+                                    </TableContainer>
+                                </PerfectScrollbar>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25]}
+                                    component="div"
+                                    count={userSt.users.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                />
                             </ThemeConfig>
                         </Box>
                     </Fade>
