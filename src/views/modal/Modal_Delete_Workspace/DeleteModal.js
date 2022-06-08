810d175283857ba2d5d1e05bd637f7595e616455
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-06-08 21:40:22 +0100

    delete

--- src/views/modal/Modal_Delete_Workspace/DeleteModal.js
+++ src/views/modal/Modal_Delete_Workspace/DeleteModal.js
@@ -98,7 +98,7 @@ const DeleteWorkspace = (props) => {
         axios
             .post( configData.API_SERVER + 'api/Workspace/deleteworkspace',{
                 token:account.token,
-                superior_id:props.card._id,
+                id:props.card._id,
                 user_id:account.user._id,
                 visualise,
                 WorkspaceName:props.card.WorkspaceName,
